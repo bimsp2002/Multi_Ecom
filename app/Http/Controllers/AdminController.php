f37d5aec50f30559140664d98a53f6421a1ed724
bimsp@2002 <bimalapaneru66@gmail.com>
2024-08-20 22:56:56 +0545

    profile pic added

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard()
+    {
+
+        return view('admin.index');
+    } // End Mehtod 
+
+    public function AdminLogin()
+    {
+        return view('admin.admin_login');
+    } // End Mehtod 
+
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }// End Mehtod 
+
+    //profile admin
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+    } // End Mehtod 
+}
