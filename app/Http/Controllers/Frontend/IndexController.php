fb1ee221371a77dca82f166465cac93b35ea8191
bimsp@2002 <bimalapaneru66@gmail.com>
2024-09-27 22:32:17 +0545

    home section

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -17,19 +17,14 @@ class IndexController extends Controller
     {
 
         $product = Product::findOrFail($id);
-
         $color = $product->product_color;
         $product_color = explode(',', $color);
-
         $size = $product->product_size;
         $product_size = explode(',', $size);
-
         $multiImage = MultiImg::where('product_id', $id)->get();
-
         $cat_id = $product->category_id;
         $relatedProduct = Product::where('category_id', $cat_id)->where('id', '!=', $id)->orderBy('id', 'DESC')->limit(4)->get();
-
-        return view('frontend.product.product_details', compact('product', 'product_color', 'product_size', 'multiImage', 'relatedProduct'));
+        return view('frontend.product.product_details', compact('product', 'product_color', 'product_size', 'multiImage','relatedProduct'));
     } // End Method 
 
 
